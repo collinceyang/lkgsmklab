0d91ec1b29501da6c22fc634557fbe150a354021
collinyang <ceyangwork@icloud.com>
2025-02-17 10:45:22 -0500

    for new web app restweb

--- startup2.sh
+++ startup2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pwd
 ls
-python -m venv antenv
-source /home/site/wwwroot/antenv/bin/activate
-python -m pip install --upgrade pip
-pip install setup
-pip install -r requirements.txt
+rm -rf antenv  # Remove old virtual environment
+python -m venv antenv  # Create new virtual environment
+source antenv/bin/activate  # Activate virtual environment
+python -m pip install --upgrade pip  # Upgrade pip
+pip install -r requirements.txt  # Install dependencies
+python --version
 uvicorn app:app --host 0.0.0.0 --port 8000 --workers 4

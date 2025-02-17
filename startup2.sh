b89eeb0a17b7ffea21f7a30c6ccd2197884f389f
collinyang <ceyangwork@icloud.com>
2025-02-17 10:13:50 -0500

    update install python in startup2.sh

--- startup2.sh
+++ startup2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 pwd
 ls
+python -m venv antenv
 source /home/site/wwwroot/antenv/bin/activate
+python -m pip install --upgrade pip
+pip install setup
+pip install -r requirements.txt
 uvicorn app:app --host 0.0.0.0 --port 8000 --workers 4

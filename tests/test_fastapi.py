fde6c83850c2aeb68e2b6f5225eb58268b4d03ca
collinyang <ceyangwork@icloud.com>
2025-02-23 18:43:21 -0500

    add test rest api

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -9,7 +9,7 @@ end_points = ["get_hostname",
                "get_ror"]
 
 for end_point in end_points:
-    url = f"http://localhost:80/{end_point}"
+    url = f"http://localhost:8000/{end_point}"
     print("\n")
     response = requests.get(url)
 
@@ -21,14 +21,14 @@ for end_point in end_points:
         print("Error:", response.status_code, response.text)
 
 end_point = "list_sku"
-url = f"http://localhost:80/{end_point}"
+url = f"http://localhost:8000/{end_point}"
 print("\n")
 response = requests.get(url)
 if response.status_code == 200:
     sku_list = response.json()["sku_list"]
     print(sku_list)
     for sku in sku_list:
-        url = f"http://localhost:80/get_sku_smk/{sku}"
+        url = f"http://localhost:8000/get_sku_smk/{sku}"
         resp = requests.get(url)
         if resp.status_code == 200:
             json_str = json.dumps(resp.json(), indent=4)

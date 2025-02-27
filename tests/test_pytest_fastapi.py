4785f9083472f7c109d91e5e7b2ee43413c7df2f
collinyang <ceyangwork@icloud.com>
2025-02-27 07:13:29 -0500

    conftest.base_api_url

--- tests/test_pytest_fastapi.py
+++ tests/test_pytest_fastapi.py
@@ -8,7 +8,7 @@ import conftest
 
 # function to get rest api response
 def get_rest_api_response(end_point):
-    url = f"{api_base_url}/{end_point}"
+    url = f"{conftest.api_base_url}/{end_point}"
     response = requests.get(url)
     if response.status_code == 200:
         json_str = json.dumps(response.json(), indent=4)
@@ -26,7 +26,7 @@ def set_ror_api(timestamp1):
     amdgpuid = timestamp1
     rocmid = str(timestamp2)
     set_endpoint =  "set_ror"
-    url = f"{api_base_url}/{set_endpoint}/{amdgpuid}_{rocmid}"
+    url = f"{conftest.api_base_url}/{set_endpoint}/{amdgpuid}_{rocmid}"
     # print(url)
     # response = requests.post(url)
     response = requests.put(url)
@@ -46,7 +46,7 @@ def get_set_ror_api(timestamp1):
     rocmid = str(timestamp2)
     get_endpoint =  "get_ror"
     set_ror_api(timestamp1) 
-    url = f"{api_base_url}/{get_endpoint}"
+    url = f"{conftest.api_base_url}/{get_endpoint}"
     response = requests.get(url)
     if response.status_code == 200:
         # json_str = json.dumps(response.json(), indent=4)
